5548be1a04af902c992c1ccdba9b4283192a4eb2
Sudha <sudha@Sudhas-MacBook-Air.local>
2024-02-25 10:50:13 +0530

    adding vercel link

--- server/index.js
+++ server/index.js
@@ -31,13 +31,13 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-// app.use (cors (
-//   {
-//   origin: ["https://deploy-mern-1whq.vetcel.app"],
-//   methods: ["POST", "GET"],
-//   credentials: true
-//   }
-//   ));
+app.use (cors (
+  {
+  origin: ["https://neatfeed-server.vercel.app"],
+  methods: ["POST", "GET"],
+  credentials: true
+  }
+  ));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
