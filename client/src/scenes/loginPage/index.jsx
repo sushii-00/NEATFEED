02d236e8c492f91c55523bbed82a9c9cf9cd9fab
Sudha <sudha@Sudhas-MacBook-Air.local>
2024-02-25 11:32:10 +0530

    fixing spelling error

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -25,7 +25,7 @@ const LoginPage = () => {
         backgroundColor={theme.palette.background.alt}
       >
         <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
-          Welcome to NEATFEED, the Social Media for Neat Freaks, define & run with you aesthetics !
+          Welcome to NEATFEED, the Social Media for Neat Freaks, define & run with your aesthetics !
         </Typography>
         <Form />
       </Box>
